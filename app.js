cac35f73d36329e2efed536d2374d8f1ae20d7a8
Joeychinedu <chinedujoey16@gmail.com>
2023-03-07 07:27:15 +0100

    another hope

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const request = require("request");
 const https = require("https");
 const path = require("path");
 require("dotenv").config({ path: "./vars/.env" });
-console.log(process.env);
 
 const app = express();
 
